32a0f12dc36d923900fd91783a9e6b0981fe0a13
jeppeter <jeppeter@gmail.com>
2024-07-12 11:15:32 +0800

    to add timeop

--- src/timeop.rs
+++ src/timeop.rs
@@ -60,3 +60,57 @@ pub fn tran_time_utc_from_str(ins :&str) -> Result<DateTime<Utc>,Box<dyn Error>>
 		}
 	}
 }
+
+
+
+
+
+
+#[cfg(target_os = "linux")]
+use libc::{clock_gettime,CLOCK_MONOTONIC_COARSE,timespec};
+
+#[cfg(target_os = "windows")]
+use winapi::um::sysinfoapi::*;
+
+pub const MAX_U64_VAL :u64 = 0xffffffffffffffff;
+
+#[cfg(target_os = "linux")]
+pub fn get_cur_ticks() -> u64 {
+	let mut  curtime = timespec {
+		tv_sec : 0,
+		tv_nsec : 0,
+	};
+	unsafe {clock_gettime(CLOCK_MONOTONIC_COARSE,&mut curtime);};
+	let mut retmills : u64 = 0;
+	retmills += (curtime.tv_sec as u64 )  * 1000;
+	retmills += ((curtime.tv_nsec as u64) % 1000000000) / 1000000;
+	return retmills;
+}
+
+#[cfg(target_os = "windows")]
+pub fn get_cur_ticks() -> u64 {
+	let retv :u64;
+	unsafe {
+		retv = GetTickCount64() as u64;
+	}
+	return retv;
+}
+
+
+pub fn time_left(sticks : u64,cticks :u64, leftmills :i32) -> i32 {
+	let eticks = sticks + leftmills as u64;
+	if cticks < eticks && cticks >= sticks {
+		return (eticks - cticks) as i32;
+	}
+
+	if (MAX_U64_VAL - sticks) < (leftmills as u64) {
+		if cticks > 0 && cticks < (leftmills as u64 - (MAX_U64_VAL - sticks)) {
+			return ((leftmills as u64) - (MAX_U64_VAL - sticks) - cticks) as i32;
+		}
+
+		if cticks >= sticks && cticks < MAX_U64_VAL {
+			return ((leftmills as u64) - (cticks - sticks)) as i32;
+		}
+	}
+	return -1;
+}
